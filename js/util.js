0b3d48d39993869c0b83475557d89c493ff1a8ad
luis <luiscarvajal2693@gmail.com>
2018-11-14 01:06:14 -0500

    initial abstraction

---
+++ js/util.js
@@ -0,0 +1,17 @@
+function goTo(section) {
+    var cfg = {
+        scrollDuration : 800
+    };
+
+    var target = this.hash,
+        $target    = $('#'+section);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 72
+    }, cfg.scrollDuration, 'swing', function () {
+    });
+}
+
+function checkIfMobile() {
+    return $('html body').width() <= 768;
+}
